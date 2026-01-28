f3b015b00a106fdb331e8df0a67378a408f86ced
AtharvSankulwar <atharvsankulwar@gmail.com>
2026-01-28 10:42:14 +0530

    Add files via upload

---
+++ exp3.1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+int a,b;
+printf("Enter first number: ");
+scanf("%d",&a);
+printf("Enter second number: ");
+printf("Difference between given two numbers: %d", (a-b));
+
+return 0;
+}
